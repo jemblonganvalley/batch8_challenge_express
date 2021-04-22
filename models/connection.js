1e87da10f2aa94b8744b43d0201f5f030c972072
jemblonganvalley <jemblonganvalley@gmail.com>
2021-04-23 04:06:54 +0700

    up

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,7 @@ const path = require("path");
 const db = require("knex")({
   client: "sqlite",
   connection: {
-    filename: path.resolve(__dirname, "../../db.sqlite"),
+    filename: path.resolve(__dirname, "../db.sqlite"),
   },
 });
 
